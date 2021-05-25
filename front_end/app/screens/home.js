e45c04b72fa9590b4cd75e41447ed48532a83143
naaoufal <naoufelbenmensour@gmail.com>
2021-05-25 15:13:07 +0100

    add files

--- front_end/app/screens/home.js
+++ front_end/app/screens/home.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 //import React, { Component } from 'react-native'
-import { Button, StyleSheet, Text, View, Dimensions, TextInput, ScrollView } from 'react-native';
+import { Button, StyleSheet, Text, View, Dimensions, TextInput, ScrollView, Image } from 'react-native';
 import * as Google from "expo-google-app-auth";
 import Constants from 'expo-constants';
 const {height, width} = Dimensions.get('screen');
@@ -10,10 +10,7 @@ import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-ta
 import { useEffect } from 'react';
 import DropDownPicker from 'react-native-dropdown-picker';
 import NavBar, { NavButton, NavButtonText, NavTitle } from 'react-native-nav';
-import { DataTable } from 'react-native-paper';
-import AddBox from "@material-ui/icons/AddBox";
-import ArrowDownward from "@material-ui/icons/ArrowDownward";
-import Modal from 'react-native-modal';
+
 
 export default function Home () {
 
@@ -32,7 +29,7 @@ export default function Home () {
     //console.log(user.uid)
 
     function renderCoinData () {
-        fetch("http://api.coincap.io/v2/assets").then(res => {
+        fetch("http://api.coincap.io/v2/assets?limit=15").then(res => {
             return res.json()
         })
         .then(info => {
@@ -48,16 +45,17 @@ export default function Home () {
     
 
     function toWallet () {
-        fetch("http://192.168.1.137:8080/user/userWallet").then(res => {
+        fetch("http://192.168.11.104:8080/user/userWallet").then(res => {
             return res.json()
         }).then(info => {
             //console.log(info)
             info.map(i => {
                 //console.log(i.f_uid)
                 if(user.uid == i.f_uid){
-                    console.log(i)
+                    console.log("kayn")
                     history.push("/Wallet")
                 } else {
+                    console.log("makaynch")
                     history.push("/Wallet")
                 }
                 // if(user.uid != i.f_uid) {
@@ -80,6 +78,11 @@ export default function Home () {
         //history.push("/Wallet")
     }
 
+    // get Graph function
+    function getGraph (rank) {
+        //console.log(rank)
+    }
+
     // logOut function :
     function logOut () {
         firebase.signOut().then(() => {
@@ -92,7 +95,7 @@ export default function Home () {
     }, [])
 
     return (
-        <View>
+        <ScrollView>
             <NavBar>
             <NavTitle>
             {user.email}
@@ -108,32 +111,52 @@ export default function Home () {
             </NavButtonText>
             </NavButton>
             </NavBar>
-            <ScrollView style={styles.scrollView}>
-            <View style={styles.container}>
                 {names.map((i) => (
-                    <View>
-                        <Text style={styles.txt}>
-                            {i.rank} # {i.name} # {i.symbol} # {i.priceUsd} # {i.changePercent24Hr}
-                        </Text>
+                    <View style={styles.listItem}>
+                        <Image source={{uri: `https://assets.coincap.io/assets/icons/${i.symbol.toLowerCase()}@2x.png`}}  style={{width:40, height:40,borderRadius:30}} />
+                        <View style={{justifyContent:"center",alignItems:"flex-start",flex:1,marginHorizontal: "5%"}}>
+                            <Text style={{fontWeight:"bold"}}>{i.name}</Text>
+                            <Text >{i.symbol}</Text>
+                        </View>
+                        <View style={{justifyContent:"center",alignItems:"center",flex:1}}>
+                            <Text style={{fontWeight:"bold"}}>${parseFloat(i.priceUsd).toFixed(2)}</Text>
+                        </View>
+                        <View style={{justifyContent:"center",alignItems:"center",flex:1}}>
+                            <Text style={{fontWeight:"bold"},
+                            
+                            i.changePercent24Hr > 0
+                                    ? {  color: "green" }
+                                    : {  color: "red" }
+                            
+                            }>{parseFloat(i.changePercent24Hr).toFixed(2)} %</Text>
+                        </View>
                     </View>
                 ))}
-            </View>
-            </ScrollView>
-        </View>
+        </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
-    container : {  padding: 16, paddingTop: 30, backgroundColor: '#fff', borderColor : 'black' },
-    head: { height: 40, backgroundColor: '#f1f8ff' },
-    text: { margin: 6 },
-    txt : {
-        color : 'black',
-        borderColor : "green",
-        borderWidth : 2,
-        padding : 15,
-        borderRadius : 8,
-        margin : 10,
-        backgroundColor : 'gray'
+    container: {
+        flex: 1,
+        backgroundColor: '#FFFFFF',
+        marginTop:0
+    },
+    listItem:{
+        margin:10,
+        padding:20,
+        backgroundColor:"#FFF",
+        width:"90%",
+        flex:1,
+        alignSelf:"center",
+        flexDirection:"row",
+        borderRadius:5,
+        elevation: 3,
+        shadowOffset: { width: 1 , height:1},
+        shadowColor: "#333",
+        shadowOpacity: 0.2,
+        justifyContent: 'center',
+        alignItems: 'center'
+    
     }
 });
